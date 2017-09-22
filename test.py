e721cf774d512eac2f524834f4954732e511026f
sethg@ipi.org <sethg@ipi.org>
2017-09-21 19:21:11 -0500

    remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-async def sent_message(msg):
-  print(mg)
-
-with open("last_trace.log",'r') as tracefile:
-    brk = tracefile.read()
-    await send_message("I just came online. Last error: \n" + brk)
