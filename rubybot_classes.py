5c7ee168da18391bcb2a833a262103a81c8c2f8b
giovanh <sethg@ipi.org>
2017-09-24 11:34:14 -0500

    Reorganized folders.
    Also did some work on the classes file, but that's not ready yet

--- rubybot_classes.py
+++ rubybot_classes.py
@@ -3,11 +3,19 @@ from discord.ext import commands
 
 class Command:
     """A simple example class"""
-    def __init__(self):
+    def __init__(self,cb,permlevel):
       self.data = []
 
 class Server:
-    """A simple example class"""
+    """Represents an instance of an actionable rubybot class
+    Arguments:
+    client : A discord.client instance
+    a      : A string, int, or discord.server.Server object
+    Provides:
+    server   : a discord.server.Server object
+    member   : a discord.Member representing client's member in the server
+    modroles : User-defined list of mod roles
+    commands : A list of rubybot command instances"""
     def __init__(self,client,a):
         try:
             if str(type(a)) == "<class 'int'>":
@@ -24,7 +32,7 @@ class Server:
         self.member = a.get_member(client.user.id)
         self.modroles = []
         self.commands = []
-        
+
     def add_cmd(self,c):
         if str(type(c)) == "<class 'rubybot_classes.Command'>":
             self.commands.append(c)
