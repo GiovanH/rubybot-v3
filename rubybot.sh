1bd9ef2871a7ffe602a0a8a4c9e27f257513dce5
giovanh <sethg@ipi.org>
2017-12-08 15:35:27 -0600

    push

--- rubybot.sh
+++ rubybot.sh
@@ -4,11 +4,11 @@ cp rubybot_v3.py rubybot_v3.pre.bak
 
 while true
 do
+	echo Reloading git
+	sh git.sh
 	echo starting up at `date`
 	python3 rubybot_v3.py
 	echo Crashed at `date`
 	cat "last_trace.log"
 	cp rubybot_v3.py rubybot_v3.post.bak
-	echo Reloading git
-	sh git.sh
 done
