909cf98be913be192c270bc12085a7b0330838d1
sethg@ipi.org <sethg@ipi.org>
2018-07-29 21:50:07 -0500

    Updated git script

--- git.sh
+++ git.sh
@@ -1,7 +1,8 @@
-#/bin/bash
-git commit frogs.frog rules rules/* -m "routine frog update"
-git push
+#/bin/bash -v
 git fetch --all
-git reset --hard origin/master
+git commit frogs.frog rules rules/* -m "routine frog update"
+git pull
+git push
+#git reset --hard origin/master
 #chmod +777 -R *
 git show > git.log
