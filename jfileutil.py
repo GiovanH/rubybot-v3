33811003bdc482d127c454bb033166270fd7986e
Gio <gio@utdallas.edu>
2018-07-30 23:00:37 -0500

    Updated jfileutil

--- jfileutil.py
+++ jfileutil.py
@@ -1,12 +1,57 @@
 import json
+import pickle
+from os import makedirs
+
+# Version 1.21
+
+basepath = "./jobj/"
+basepath_json = basepath
+basepath_pick = basepath
+
+
+def ensure_dirs(destpath):
+    # Make necessary directories, if they don't exist.
+    try:
+        makedirs(destpath)
+    except FileExistsError:
+        pass
 
 
 def load(filename):
-    with open("jobj/" + filename + ".json", 'r') as file:
-        return json.load(file)
+    return json_load(filename)
 
 
 def save(object, filename):
+    return json_save(object, filename)
+
+
+def pickle_to_json(filename):
+    json_save(pickle_load(filename), filename)
+
+
+def json_to_pickle(filename):
+    pickle_save(json_load(filename), filename)
+
+
+def json_load(filename):
+    with open(basepath_json + filename + ".json", 'r') as file:
+        return json.load(file)
+
+
+def json_save(object, filename):
+    ensure_dirs(basepath_json)
     j = json.dumps
-    with open("jobj/" + filename + ".json", 'w') as file:
+    with open(basepath_json + filename + ".json", 'w') as file:
         j = json.dump(object, file, indent=4)
+
+
+def pickle_load(filename):
+    filehandler = open(basepath_pick + filename + ".obj", 'rb')
+    object = pickle.load(filehandler)
+    return object
+
+
+def pickle_save(object, filename):
+    ensure_dirs(basepath_pick)
+    filehandler = open(basepath_pick + filename + ".obj", 'wb')
+    pickle.dump(object, filehandler)
